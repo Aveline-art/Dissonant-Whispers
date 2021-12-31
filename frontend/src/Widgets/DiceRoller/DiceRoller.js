4aca64bea927eb43f53a1f28bce9cde7d895cda1
avali4907 <avali4907@outlook.com>
2021-12-30 21:50:35 -0800

    added empty dice roller

---
+++ frontend/src/Widgets/DiceRoller/DiceRoller.js
@@ -0,0 +1,54 @@
+import { useReducer } from "react";
+
+function DiceRoller() {
+  
+
+  return (
+    <div className="container">
+      <div className="row my-5">
+        <div className="col-2 btn-group-vertical">
+          
+        </div>
+        <div className="col-6">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function DieCounter({ die, ...props }) {
+  return (
+    <div
+      className={`btn-group ${props.className}`}
+      role="group"
+      aria-label="First group"
+    >
+      <button type="button" className="btn btn-primary col-4">
+        {die.name}
+      </button>
+      <button
+        onClick={() => {
+          props.dispatch({ index: props.index, type: "increment" });
+        }}
+        type="button"
+        className="btn btn-outline-secondary col-2"
+      >
+        +
+      </button>
+      <button
+        onClick={() =>
+          props.dispatch({ index: props.index, type: "decrement" })
+        }
+        type="button"
+        className="btn btn-outline-secondary col-2"
+      >
+        -
+      </button>
+      <button type="button" className="btn btn-outline-secondary col-4">
+        Table
+      </button>
+    </div>
+  );
+}
+
+export default DiceRoller;
