49e18d71fea20d97d1490e04c3d7b5409d156b15
avali4907 <avali4907@outlook.com>
2021-12-31 20:39:47 -0800

    stored total and rolls state

--- frontend/src/Widgets/DiceRoller/DiceRoller.js
+++ frontend/src/Widgets/DiceRoller/DiceRoller.js
@@ -1,10 +1,33 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 function DiceRoller() {
   const dice = [
-    useState({ name: "d4", count: 0 }),
-    useState({ name: "d6", count: 0 }),
+    useState({ name: "d4", count: 0, sides: 4 }),
+    useState({ name: "d6", count: 0, sides: 6 }),
   ];
+  const [total, setTotal] = useState(0);
+  const [results, setResults] = useState([]);
+
+  function rollDice(dice) {
+    let total = 0;
+    let allRolls = [];
+    for (const die of dice) {
+      let rolls = {
+        die: die[0].name,
+        rolls: [],
+      };
+
+      for (let i = 0; i < die[0].count; i++) {
+        const roll = Math.ceil(Math.random() * die[0].sides);
+        rolls.rolls.push(roll);
+        total += roll;
+      }
+
+      allRolls.push(rolls);
+    }
+    setTotal(total)
+    setResults(allRolls)
+  }
 
   return (
     <div className="container">
@@ -13,6 +36,24 @@ function DiceRoller() {
           {dice.map((dieState, index) => {
             return <DieCounter key={index} die={dieState} />;
           })}
+        </div>
+      </div>
+      <div className="row my-2">
+        <button
+          type="button"
+          className="btn btn-primary col-2"
+          onClick={() => rollDice(dice)}
+        >
+          Roll!
+        </button>
+        <div className="col-2">
+          <input
+            className="form-control form-control-sm"
+            type="text"
+            value={total}
+            aria-label=".form-control-sm example"
+            disabled
+          ></input>
         </div>
       </div>
     </div>
@@ -39,7 +80,7 @@ function DieCounter(props) {
 
   return (
     <div
-      class="btn-toolbar"
+      className="btn-toolbar"
       role="toolbar"
       aria-label="Toolbar with button groups"
     >
@@ -71,7 +112,7 @@ function DieCounter(props) {
         aria-label="First group"
       >
         <input
-          class="form-control form-control-sm"
+          className="form-control form-control-sm"
           type="text"
           value={die.count}
           aria-label=".form-control-sm example"
