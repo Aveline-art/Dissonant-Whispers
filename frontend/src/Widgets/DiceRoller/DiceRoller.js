c28c36c0f916c207ede31360af27ff090d33cb7f
avali4907 <avali4907@outlook.com>
2021-12-31 19:25:18 -0800

    completed state

--- frontend/src/Widgets/DiceRoller/DiceRoller.js
+++ frontend/src/Widgets/DiceRoller/DiceRoller.js
@@ -1,30 +1,48 @@
-import { useReducer, useState } from "react";
-
-function useDice(list) {
-    const item = list.map((i) => {
-        return useState({name: i, count: 0})
-    })
-
-    return item
-}
+import { useState } from "react";
 
 function DiceRoller() {
-  
+  const dice = [
+    useState({ name: "d4", count: 0 }),
+    useState({ name: "d6", count: 0 }),
+  ];
 
   return (
     <div className="container">
       <div className="row my-5">
         <div className="col-2 btn-group-vertical">
-          
+          {dice.map((dieState, index) => {
+            return <DieCounter key={index} die={dieState} />;
+          })}
         </div>
         <div className="col-6">
+          {dice.map((dieState, index) => {
+            return dieState[0].count;
+          })}
         </div>
       </div>
     </div>
   );
 }
 
-function DieCounter({ die, ...props }) {
+function DieCounter(props) {
+  const [die, setDie] = props.die;
+
+  function increment(die) {
+    if (die.count < 100) {
+      const copy = Object.assign({}, die);
+      copy.count++;
+      setDie(copy);
+    }
+  }
+
+  function decrement(die) {
+    if (die.count > 0) {
+      const copy = Object.assign({}, die);
+      copy.count--;
+      setDie(copy);
+    }
+  }
+
   return (
     <div
       className={`btn-group ${props.className}`}
@@ -35,18 +53,14 @@ function DieCounter({ die, ...props }) {
         {die.name}
       </button>
       <button
-        onClick={() => {
-          props.dispatch({ index: props.index, type: "increment" });
-        }}
+        onClick={() => increment(die)}
         type="button"
         className="btn btn-outline-secondary col-2"
       >
         +
       </button>
       <button
-        onClick={() =>
-          props.dispatch({ index: props.index, type: "decrement" })
-        }
+        onClick={(e) => decrement(die)}
         type="button"
         className="btn btn-outline-secondary col-2"
       >
